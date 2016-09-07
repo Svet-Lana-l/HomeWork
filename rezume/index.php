7f3b4e6960ad722f23748d3e4724391063b18569
Svet-Lana <lazor-svet@ukr.net>
2016-09-07 11:32:44 +0300

    ver-1-0

---
+++ rezume/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="uk">
+	<head>
+		<title>My rezume</title>
+		<meta charset="utf-8"/>
+		<meta name="keywords" content="rezume"/>
+		<meta name="description" content="The my Web-site"/>
+        <link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<!-- My rezume -->
+		<h1>Резюме</h1>
+		<h2>Лазоренко Світлана Костянтинівна</h2>
+		<hr>
+		<hr>
+		<div class="side_right">
+			<img src="img/foto.jpg" width="110" alt="Фото">
+		</div>
+		<div class="lichn">
+			<table>
+				<tr>
+					<td class="t_right">Дата народження:  </td>
+					<td class="t_left">02.02.1968р.</td>
+				</tr>
+				<tr>
+					<td class="t_right" >Сімейний стан:</td>
+					<td class="t_left">одружена</td>
+				</tr>
+				<tr>
+					<td class="t_right">Адреса:</td>
+					<td class="t_left">м.Київ, Дарницький р-н, вул. Бориспільська</td>
+				</tr>
+				<tr>
+					<td rowspan="2" class="t_right">тел.</td>
+					<td class="t_left">379-14-24</td>
+				</tr>
+				<tr>
+					<td class="t_left">050-202-26-46</td>
+				</tr>
+				<tr>
+					<td class="t_right">e-mail: </td>
+					<td class="t_left">Lazor-svet@ukr.net</td>
+				</tr>
+			</table>
+		</div>
+		<hr>
+			<h3>МЕТА</h3>
+				<p><i>Заміщення вакантної посади <b>програміста.</b> </i></p>
+			<h3>ОСВІТА </h3>
+				<table class="robota">
+                    <tr>
+					    <td class="tab1_right">1985 р.</td>
+                         <td>закінчила Київську фізико-математичну школу № <b>145 </b></td>
+                    </tr>
+                    <tr>
+					    <td class="tab1_right">1985 - 1991 р.</td>
+                        <td>навчалася в <b><i>Київському Політехнічному Інституті</i></b>
+                        <img src="img/kpi_0.png" alt="logo"/> на факультеті<b> ФЕТ</b>	за спеціальністю
+                        <b>“САПР”</b>, який закінчила з червоним дипломом, отримавши кваліфікацію
+						<b><i>інженер-системотехнік</i></b>  </td>
+                    </tr>
+                    <tr>
+					    <td class="tab1_right">2016 р.</td>
+                        <td>курси <b>WEB</b>-програмування.</td>
+                    </tr>
+				</table>
+			<h3>ТРУДОВА ДІЯЛЬНІСТЬ </h3>
+				<table class="robota">
+					<tr>
+						<td rowspan="2" class="tab2_right">1991 - 1996 р.</td>
+						<td> <b>КБ Київського радіозавода</b></td>
+					</tr>
+					<tr>
+						<td><i>Інженер-програміст</i></td>
+					</tr>
+					<tr>
+						<td rowspan="2" class="tab2_right"> 1996 – 2016 p.</td>
+							<td><b><img src="img/bank.jpg" alt="logo"/> Дарницьке відділення АТ „ Ощадбанк”</b></td>
+					</tr>
+					<tr>
+						<td><i>Заступник начальника відділу інформатики та автоматизації.</i></td>
+					</tr>
+				</table>
+			<h3>ПРОФЕСІЙНІ НАВИЧКИ </h3>
+				<ul>
+					<li> Мови програмування: Clipper, FoxPro, Visual Basic для Excel.
+						Вивчаю Html, Css, Java-script, Php </li>
+					<li>Робота з БД</li>
+					<li>Встановлення, налаштування та заміна ОС Windows 9х, XP, Seven.
+						Інсталяція та налаштування необхідного програмного забезпечення до них.</li>
+					<li>Адміністрування локальної мережі, серверів (Windows 2003 Server, Free BSD,
+						віртуальних серверів, термінальний сервер).</li>
+					<li>Работа в домені зі службою Active Directory – додавання комп’ютерів та користувачів в домен,
+						розблокування, призначення прав . </li>
+					<li>Інсталяція, тестування і супровід  програм.</li>
+					<li>Підтримка користувачів</li>
+				</ul>
+			<h3>ОСОБИСТІ ЯКОСТІ</h3>
+			<ul>
+				<li>Ретельність</li>
+				<li>Коммуникативність</li>
+				<li>Пунктуальність</li>
+				<li>Легко та із задоволенням навчаюсь</li>
+				<li>Стресостійкість </li>
+				<li>Відсутність поганих звичок</li>
+				<li>Висока працездатність</li>
+			</ul>
+		<hr>
+		<footer>
+			<table>
+				<tr>
+					<td><a href="https://www.linkedin.com" target="_blank"><img src="img/linkedin.jpg" alt="LinkedIn"></a></td>
+					<td><a href="https://www.skype.com/ru/" target="_blank"><img src="img/skype.jpg" alt="Skype"></a></td>
+				</tr>
+			</table>
+		</footer>
+	</body>
+ </html>
