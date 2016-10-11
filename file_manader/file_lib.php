c50c3e2e9c8e9721e192d52dbf78d5bd7971bb7c
Svet-Lana <lazor-svet@ukr.net>
2016-10-11 16:00:21 +0300

    ver-2-0

---
+++ file_manader/file_lib.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sveta
+ * Date: 07.10.2016
+ * Time: 22:04
+ */
+
+
+echo '<br>';
+
+if (isset($_POST['file_text'])) {
+    save_file();
+}
+function if_file_dir($files, $dir_c){
+
+    $ident_f = '';
+    if ($files == '.') {
+        $ident_f = 'a';
+    }
+    elseif ($files == '..') {
+        $ident_f = 'b';
+    }
+    elseif (!file_exists($dir_c . df. $files) or !is_writable($dir_c . df. $files)
+        or !is_readable($dir_c . df. $files)) {
+        $ident_f = 'h';
+    }
+    elseif (is_file($dir_c . df. $files)){
+        $ident_f = 'f';
+    }
+    elseif (is_dir($dir_c . df. $files)) {
+        $ident_f = 'd';
+    }
+    elseif (is_link($dir_c . df. $files)) {
+        $ident_f = 'l';
+    }
+
+
+    else {
+        $ident_f = 'h';
+    }
+
+    return $ident_f;
+}
+
+function get_dir($arr_dir, $dir_c) {
+
+    $stroka = '';
+    echo '<table>';
+    foreach ($arr_dir as $files) {
+
+        switch (if_file_dir($files, $dir_c)) {
+
+            case 'a':
+                $stroka ='   ' . $files;
+                break;
+            case 'b':
+                $dir_new = dirname($dir_c);
+                $stroka = '<a href=?dir=' . $dir_new . '><img src="img/arrow_up.png" class="icon"> ' . $files . '</a>';
+                break;
+            case 'd':
+                $dir_new = trim($dir_c) . df . trim($files);
+                $stroka = '<a class="active_file" href=?dir=' . $dir_new . '> 
+                <img src="img/folder.png" class="icon"> ' . $files . '</a>';
+                break;
+            case 'f':
+                $dir_new = trim($dir_c) . df . trim($files);
+                $stroka = '<a class="active_file" href="?name=' . $dir_new .'&dir=' . $dir_c . '#edit">
+                <img src="img/document.png" class="icon"> ' . $files. '</a>';
+                break;
+            case 'h':
+            case 'l':
+                $dir_new = $dir_c;
+                $stroka = '<a class="active_file" href=?dir=' . $dir_new . '> 
+                <img src="img/folder-locked.png" class="icon"> ' . $files . '</a>';
+                break;
+
+        }
+        echo '<tr><td>' . $stroka . '</td></tr>';
+    }
+    echo '</table>';
+}
+
+function save_file(){
+
+    if (isset($_POST['name']) and isset($_POST['file_text'])) {
+        file_put_contents($_POST['name'], $_POST['file_text']);
+    }
+   //$dir_c = $_POST['name'];
+
+   header('Location:index.php');
+}
---
+++ file_manader/index.php
@@ -0,0 +1,116 @@
+<?php
+//if ($_POST) {
+  //  echo $_SERVER['REQUEST_URI'];
+    // header('Location:'.$_SERVER['REQUEST_URI']);
+//}
+require_once 'file_lib.php';
+
+//$dir_root = __DIR__;
+define('df' , DIRECTORY_SEPARATOR);
+
+if (isset($_GET['dir'])) {
+    if (is_dir($_GET['dir'])) {
+        $dir_c = $_GET['dir'];
+    }
+    else{
+        if (!isset($dir_c)){
+            $dir_c = __DIR__;
+        }
+    }
+}
+elseif (isset($_POST['dir'])) {
+    if (is_dir($_POST['dir'])) {
+        $dir_c = $_POST['dir'];
+    }
+    else{
+        if (!isset($dir_c)) {
+            $dir_c = __DIR__;
+        }
+    }
+}
+else {
+    if ((!isset($dir_c)) ){
+        $dir_c = __DIR__;
+    }
+}
+
+$arr_dir = scandir($dir_c);
+asort($arr_dir);
+$arr_d = array();
+foreach ($arr_dir as $key=>$value){
+    $key1 = if_file_dir($value, $dir_c) . $key;
+    $arr_d[$key1] = $value;
+}
+ksort($arr_d);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <title>File_Manager</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link type='text/css' rel='stylesheet' href='css/style.css'/>
+    <script src="CodeMirror/cm/lib/codemirror-compressed.js"></script>
+    <script src="CodeMirror/cm/mode/javascript-compressed.js"></script>
+    <link rel="stylesheet" href="CodeMirror/cm/lib/codemirror.css">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins)-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        if (document.getElementById("code")) {
+        var editor = CodeMirror.fromTextArea(document.getElementById("code"),  {
+            lineNumbers: true,               // показывать номера строк
+            matchBrackets: true,             // подсвечивать парные скобки
+            mode: 'application/x-httpd-php', // стиль подсветки
+            indentUnit: 4                    // размер табуляции
+        });}
+
+    </script>
+</head
+
+<body>
+<h1>File-Manager</h1>
+    <div class="manager-all">
+        <section id="section1">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 decor">
+
+                        <?php get_dir($arr_d, $dir_c);?>
+
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12" id="edit">
+
+                    <?php
+                        if (isset($_GET['name'])) {
+                            $text_file = file_get_contents($_GET['name']);
+                            $name_file = $_GET['name'];
+                    ?>
+                            <form action="file_lib.php" method="post">
+                                <label for="code">Редактируем файл - <?php echo $name_file;?></label>
+                                <textarea name="file_text" id="code"><?php echo $text_file; ?></textarea>
+                                <input type="hidden" name="name" value="<?php echo $name_file;?>">
+                                <input type="hidden" name="dir" value="<?php echo $dir_c;?>">
+                                <input type="submit" value="Save">
+                            </form>
+                            <div id="post_save">
+                            <?php
+                          }
+                             ?>
+
+                             </div>
+                </div>
+            </div>
+        </section>
+       <section id="section2">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
